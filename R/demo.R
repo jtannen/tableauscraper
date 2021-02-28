8517a70e8deca8e62606052acd6c61e05c015ac2
jtannen <jonathan.tannen@gmail.com>
2021-02-28 09:55:49 -0500

    changed demo to use gen_config

--- R/demo.R
+++ R/demo.R
@@ -1,10 +1,8 @@
 
 if(FALSE){
-  CONFIG <- list(
-    host_url="https://healthviz.phila.gov/",
-    path="t/PublicHealth/views/COVIDVaccineDashboard/COVID_Vaccine",
-    req_url=":embed=y&:showVizHome=no&:host_url=https%3A%2F%2Fhealthviz.phila.gov%2F&:embed_code_version=3&:tabs=no&:toolbar=no&:alerts=no&:showShareOptions=false&:showAskData=false&:showAppBanner=false&:isGuestRedirectFromVizportal=y&:display_spinner=no&:loadOrderID=0"
+  library(tableascrape)
+  config <- gen_config(
+    "https://www.phila.gov/programs/coronavirus-disease-2019-covid-19/vaccines/data/"
   )
-
-  dfs <- scrape_tableau(CONFIG)
+  dfs <- scrape_tableau(config)
 }
