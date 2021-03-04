c39465d1d72f6bb456eac7539f087b25edca9694
jtannen <jonathan.tannen@gmail.com>
2021-03-04 07:58:50 -0500

    fix load() to requireNamespace()

--- R/scraper.R
+++ R/scraper.R
@@ -1,4 +1,4 @@
-requireNamespace(dplyr)
+requireNamespace("dplyr")
 
 # req <- "https://healthviz.phila.gov/t/PublicHealth/views/COVIDVaccineDashboard/COVID_Vaccine?:embed=y&:showVizHome=no&:host_url=https%3A%2F%2Fhealthviz.phila.gov%2F&:embed_code_version=3&:tabs=no&:toolbar=no&:alerts=no&:showShareOptions=false&:showAskData=false&:showAppBanner=false&:isGuestRedirectFromVizportal=y&:display_spinner=no&:loadOrderID=0"
 
